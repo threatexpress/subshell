baa51d988713bce2307ee160a022bf144edff601
Joe Vest <mrjoevest@gmail.com>
2016-11-06 10:00:43 -0600

    setup.py update

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name = "subshell",
     version = "1.0",
-    packages = find_packages()
+    packages = find_packages(),
+    install_requires=[
+		'docopt==0.6.2',
+		'readline==6.2.4.1',
+		'requests==2.7.0',
+		'tabulate==0.7.5'
+    ]
+
     )
